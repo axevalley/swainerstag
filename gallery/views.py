442992149c53694604484eb9979e311c32d68d5d
Luke Shiner <luke@lukeshiner.com>
2016-07-08 02:00:12 +0100

    Random

--- gallery/views.py
+++ gallery/views.py
@@ -6,7 +6,10 @@ from . models import GalleryImages
 def index(request):
     from django.conf import settings
     import os
+    import random
     media_dir = settings.MEDIA_ROOT
     thumb_folder = os.path.join(media_dir, 'thumbs')
     thumbs = [thumb for thumb in os.scandir(thumb_folder)]
-    return render(request, 'gallery/index.html', {'thumbs': thumbs})
+    random.shuffle(thumbs)
+    return render(request, 'gallery/index.html', {
+        'thumbs': thumbs})
